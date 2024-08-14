b42e14037dfca677126d7cd1502b311a03872a87
manjinder-11 <js3709500@gmail.com>
2024-08-14 01:22:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectDB();
 
 app.use(morgan("dev"));
 // app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://booksmart-frontend-z6mj.onrender.com" }));
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // app.use(bodyParser.json({ limit: "80000kb" }));
